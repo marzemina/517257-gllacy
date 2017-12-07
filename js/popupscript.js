44eeb345188265e22de6ef7719ddeb4bb5d401df
marzemina <daria.sukhoroslova@gmail.com>
2017-12-07 21:58:04 +0700

    Оживить интерактивные элементы в личном проекте
    внесение исправлений

--- js/popupscript.js
+++
@@ -1,9 +0,0 @@
-function showit() {	
-    document.getElementById("cfpopup").classList.add("feedback-form");
-	document.getElementById("cfoverlay").classList.add("feedback-form-overlay-show");
-}
-
-function closeit(){
-	document.getElementById("cfpopup").classList.remove("feedback-form");
-    document.getElementById("cfoverlay").classList.remove("feedback-form-overlay-show");
-}
